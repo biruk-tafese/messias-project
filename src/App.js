659d919b05f7e92c6b08fd1ee8e7957c08a356b5
YeabNigusse <nigusseyeabsira@gmail.com>
2023-12-23 10:52:28 +0300

    working on blog section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import NavBar from './component/navBar'
-import Home from './component/home';
-import Card from './component/cards';
+import NavBar from './component/NavBar/navBar';
+import Blog from   './component/Blogs/blogs';
+import Home from   './component/Home/home';
+import Card from   './component/Card/cards';
+
+
 function App() {
   return (
     <>
     <NavBar/>
     <Home/>
     <Card/>
+    <Blog/>
     </>
   );
 }
---
+++ src/component/Blogs/blogs.js
@@ -0,0 +1,22 @@
+import './blog.css'
+
+
+function Blog(){
+    return(
+        <>
+        <div className="parent">
+            <div className="content">
+                <h1>News and Updates</h1>
+                <p>The Harvest is a monthly publication of EvaSUE dedicated to sharing the latest news with our partners.</p>
+                <button>View all Blogs</button>
+            </div>
+
+            <div className="blogs">
+                Blogs
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Blog
