75d2d4885f0c27e31d1ebdf9f3de7606c8a0536f
YeabNigusse <nigusseyeabsira@gmail.com>
2023-12-30 17:25:51 +0300

    adding pinter in read more button

--- src/component/Card/CardItems.js
+++ src/component/Card/CardItems.js
@@ -1,13 +1,22 @@
+
+import { useNavigate  } from 'react-router-dom';
 import classes from './cardItems.module.css'
 
 export default function CardItems(props){
+
+    const navigate = useNavigate ();
+
+    const handleReadMoreClick = () => {
+        // Navigate to the "/another-page" route
+        navigate('/about');
+      };
 
     return(
             <div className={classes.card}>
                 <div className={classes.cardContent}>
                     <h1>{props.title}</h1>
                     <p>{props.discription}</p>
-                    <a href='#' className={classes.cardButton}>Read More</a>
+                    <a className={classes.cardButton} onClick={handleReadMoreClick}>Read More</a>
                 </div>
             </div>
     )
