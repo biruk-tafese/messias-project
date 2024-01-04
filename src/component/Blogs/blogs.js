0b344caa1bf37fd4ebebfd60b8221ad4a8a23b74
YeabNigusse <nigusseyeabsira@gmail.com>
2024-01-05 00:19:02 +0300

    adding some file

--- src/component/Blogs/blogs.js
+++ src/component/Blogs/blogs.js
@@ -1,35 +1,28 @@
-import './blog.css'
+// Blog.js
+import SimpleSlider from './slider'; // Assuming your file is named Slider.js
+import './blog.css';
 
-
-function Blog(){
-    return(
-        <>
-        <div className="parent">
-
-
-            <div className="content">
-                <h1>News and Updates</h1>
-                <p>The Harvest is a monthly publication of EvaSUE dedicated to sharing the latest news with our partners.</p>
-                <button>View all Blogs</button>
-            </div>
-
-            <div className="page-wrapper">
-                <div className='post-slider'>
-                    <h1 className='slider-title'>Our Blogs</h1>
-                    <div className='post-wrapper'>
-                        <div className='post'>1</div>
-                        <div className='post'>2</div>
-                        <div className='post'>3</div>
-                        <div className='post'>4</div>
-                        <div className='post'>5</div>
-                    </div>
-
-
-                </div>
-            </div>
+function Blog() {
+  return (
+    <>
+      <div className="parent">
+        <div className="content">
+          <h1>News and Updates</h1>
+          <p>The Harvest is a monthly publication of EvaSUE dedicated to sharing the latest news with our partners.</p>
+          <button>View all Blogs</button>
         </div>
-        </>
-    )
+
+        <div className="page-wrapper">
+          <div className='post-slider'>
+            <h1 className='slider-title'>Our Blogs</h1>
+            <div className='post-wrapper'>
+              <SimpleSlider />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Blog
+export default Blog;
---
+++ src/component/Blogs/slider.js
@@ -0,0 +1,42 @@
+// Slider.js
+
+import React, { Component } from "react";
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true, // Set to true for auto-advancing
+      autoplaySpeed: 2000, // Adjust the speed in milliseconds
+    };
+    return (
+      <Slider {...settings}>
+        <div className="card">
+          <h3>1</h3>
+        </div>
+        <div className="card">
+          <h3>2</h3>
+        </div>
+        <div className="card"> 
+          <h3>3</h3>
+        </div>
+        <div className="card">
+          <h3>4</h3>
+        </div>
+        <div className="card">
+          <h3>5</h3>
+        </div>
+        {/* <div className="card">
+          <h3>6</h3>
+        </div> */}
+      </Slider>
+    );
+  }
+}
