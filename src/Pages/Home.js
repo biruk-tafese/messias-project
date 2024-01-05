9904c4d6dc0d1e05df626a503050689ad3d0f631
YeabNigusse <nigusseyeabsira@gmail.com>
2024-01-06 02:47:26 +0300

    done half of the footer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import Blog from   '../component/Blogs/blogs';
 import Home from   '../component/Home/home';
 import Card from   '../component/Card/cards';
 import Register from '../component/BlogRegister/register';
+import Footer from '../component/Footer/footer';
 
 export default function HomePage(){
     return(
@@ -11,7 +12,8 @@ export default function HomePage(){
     <Home/>
     <Card/>
     <Blog/>
-    <Register/>            
+    <Register/>
+    <Footer/>          
         </>
     )
 }
