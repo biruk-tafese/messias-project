04c9379067cfdc27f7baf0b60e8ff0173be51814
YeabNigusse <nigusseyeabsira@gmail.com>
2024-01-13 11:05:16 +0300

    modified files

--- src/component/NavBar/navBar.js
+++ src/component/NavBar/navBar.js
@@ -1,23 +1,25 @@
-import { Link } from 'react-router-dom';
+import {NavLink, Link } from 'react-router-dom';
 
 import './navBar.css'
 
 function NavBar(){
     return(
         <>
-        <div className="nav">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <nav className="nav">
             <h2 className='logo'>Messia's Mission Ministry</h2>
-            <nav>
+            <div className='menu'>
+                <i className="fa fa-bars"></i>
+            </div>
                  <ul className='ul'>
                     <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/ministry'>Ministry</Link></li>
-                    <li><Link to='/about'>About</Link></li>  
-                    <li><Link to='/blog'>Blogs</Link></li>
-                    <li><Link to='/contact'>Contact Us</Link></li>
-                    <li><Link to='/donate'>Donate</Link></li>
+                    <li><NavLink to='/ministry'>Ministry</NavLink></li>
+                    <li><NavLink to='/about'>About</NavLink></li>  
+                    <li><NavLink to='/blog'>Blogs</NavLink></li>
+                    <li><NavLink to='/contact'>Contact Us</NavLink></li>
+                    <li><NavLink to='/donate'>Donate</NavLink></li>
                  </ul>
             </nav>
-        </div>
         </>
     );
 }
