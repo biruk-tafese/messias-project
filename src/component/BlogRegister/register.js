a7918b2c5d30155b38e4211b6a8eebe845a12b7a
YeabNigusse <nigusseyeabsira@gmail.com>
2024-01-14 20:47:36 +0300

    adding dome content

--- src/component/BlogRegister/register.js
+++ src/component/BlogRegister/register.js
@@ -6,8 +6,8 @@ export default function Register(){
         <div className="contents">
             <div className="discription">
                 <h1>Subscribe to Our Newsletter</h1>
-                <p> The Evangelical Students and Graduates Union of Ethiopia (EvaSUE) has many stories to share with you. A free subscription gives you the opportunity to stay tuned to our dynamic ministry through our monthly publication, 
-                    “The Harvest.” There you can find major ministry updates, information about our upcoming events, and job opportunities. </p>
+                <p> The Messiah's Mission Ministry has many stories to share with you. A free subscription gives you the opportunity to stay tuned to our dynamic ministry through our monthly publication, 
+                 There you can find major ministry updates, information about Messiah's Mission Ministry. </p>
                 </div>
             <div className="input">
                 <input placeholder='Your Email'/>
